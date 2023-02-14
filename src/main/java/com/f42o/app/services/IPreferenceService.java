4ec76c1aac2de5307abab688ff0ae3370549d5ec
federicos1 <fedes7777@gmail.com>
2023-02-14 14:58:26 -0300

    payer

--- src/main/java/com/f42o/app/services/IPreferenceService.java
+++ src/main/java/com/f42o/app/services/IPreferenceService.java
@@ -1,9 +1,11 @@
 package com.f42o.app.services;
 
 import com.f42o.app.dto.PreferenceDTO;
+import com.mercadopago.exceptions.MPApiException;
+import com.mercadopago.exceptions.MPException;
 import com.mercadopago.resources.preference.Preference;
 
 public interface IPreferenceService {
-	Preference create(PreferenceDTO dto);
+	Preference create(PreferenceDTO dto) throws MPException, MPApiException;
 
 }
--- src/main/java/com/f42o/app/services/PreferenceServiceImpl.java
+++ src/main/java/com/f42o/app/services/PreferenceServiceImpl.java
@@ -10,29 +10,39 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.f42o.app.dto.PayerDTO;
 import com.f42o.app.dto.PaymentMethodDTO;
 import com.f42o.app.dto.PreferenceDTO;
 import com.f42o.app.dto.PreferenceItemsDTO;
 import com.google.gson.Gson;
 import com.mercadopago.MercadoPagoConfig;
+import com.mercadopago.client.common.AddressRequest;
+import com.mercadopago.client.common.PhoneRequest;
+import com.mercadopago.client.common.PhoneRequest.PhoneRequestBuilder;
+import com.mercadopago.client.payment.PaymentPayerRequest;
 import com.mercadopago.client.preference.PreferenceBackUrlsRequest;
 import com.mercadopago.client.preference.PreferenceClient;
 import com.mercadopago.client.preference.PreferenceItemRequest;
+import com.mercadopago.client.preference.PreferencePayerRequest;
 import com.mercadopago.client.preference.PreferencePaymentMethodRequest;
 import com.mercadopago.client.preference.PreferencePaymentMethodsRequest;
 import com.mercadopago.client.preference.PreferenceRequest;
 import com.mercadopago.exceptions.MPApiException;
 import com.mercadopago.exceptions.MPException;
+import com.mercadopago.resources.payment.PaymentPayer;
 import com.mercadopago.resources.paymentmethod.PaymentMethod;
 import com.mercadopago.resources.preference.Preference;
+import com.mercadopago.resources.preference.PreferencePayer;
 
 @Service
-public class PreferenceServiceImpl {
+public class PreferenceServiceImpl implements IPreferenceService{
 	private static final Logger log = LoggerFactory.getLogger(PreferenceServiceImpl.class);
 	@Value("${mercadolibre.config.accessToken}")
 	private String AccessToken;
 	@Value("${mercadolibre.config.integratorId}")
 	private String integratorId;
+	
+	@Override
 	public Preference create(PreferenceDTO dto) throws MPException, MPApiException {
 
 		MercadoPagoConfig.setAccessToken(AccessToken);
@@ -69,16 +79,31 @@ public class PreferenceServiceImpl {
 		PreferencePaymentMethodsRequest preferencePaymentMethodsRequest = PreferencePaymentMethodsRequest.builder()
 				.installments(dto.getPaymentMethods().getInstallments())
 				.excludedPaymentMethods(excludedPaymentMethods).build();
+		
+		PayerDTO payerDto = dto.getPayer();
+		PreferencePayerRequest payer = 
+				PreferencePayerRequest.builder()
+				.address(AddressRequest.builder()
+						.streetName(payerDto.getAddress().getStreetName())
+						.streetNumber(payerDto.getAddress().getStreetNumber())
+						.zipCode(payerDto.getAddress().getZipCode()).build()						
+						)
+				.phone(PhoneRequest.builder()
+						.number(payerDto.getPhone().getNumber())
+						.areaCode(payerDto.getPhone().getAreaCode())
+						.build()
+						)
+				.build();
 
-		
-		
 		PreferenceRequest request = PreferenceRequest.builder()
 				.items(items)
 				.backUrls(backUrls)
+				.payer(payer)
 				.paymentMethods(preferencePaymentMethodsRequest)
 				.externalReference(dto.getExternalReference())
 				.autoReturn(dto.getAutoReturn())
 				.notificationUrl(dto.getNotificationUrl())
+				.statementDescriptor("Tienda Azul")
 				.build();
 
 		return client.create(request);
